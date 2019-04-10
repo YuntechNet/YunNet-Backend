e8f54e820bad17f67e01c33c6ea8e4a1d010ee55
Zeinok <zeinok@protonmail.com>
2019-04-10 23:23:14 +0800

    fix function naming in announce.py

--- api/announcement.py
+++ api/announcement.py
@@ -4,13 +4,13 @@ from sanic import Blueprint
 announcement = Blueprint('announcement')
 
 @announcement.route('/announcement', methods=['GET'])
-async def bp_add(request):
+async def bp_all_announcement(request):
     
     response =  json({})
     return response
 
 @announcement.route('/announcement/<ID>', methods=['GET'])
-async def bp_add(request):
+async def bp_single_announcement(request):
     
     response =  json({})
     return response
