52dc826d1fd3da5357e5004884d84b014bcd946b
biboy1999 <biboy1999@gmail.com>
2019-06-23 14:29:24 +0800

    add login api
    add iat(issued at) attribute
     -jwt payload use utc time
    fix TypeError in user.get_password function

--- api/user/login.py
+++ api/user/login.py
@@ -1,15 +1,42 @@
+import jwt
+from sanic.log import logger
 from sanic.response import json
 from sanic import Blueprint
+from sanic_openapi import doc
+
+import config
+from Base.jwt_payload import jwt_payload
+from Query.user import User
+from Query.permission import Permission
 
 login = Blueprint('login')
 
 
 @login.route('/login', methods=['POST'])
+@doc.consumes(doc.JsonBody({'username': str, 'password': str}),
+              content_type='application/json', location='body')
 async def bp_login(request):
-    # if success
-    response = json({'session_key': '00000000-0000-0000-00000000'})
-    # else
-    # response = json({'message':'Username/Password combination incorrect'},
-    #                 status=401)
-    # redirect to user info?
-    return response
+    try:
+        username = request.json['username']
+        password = request.json['password']
+
+        # check login permission
+        allowed = Permission().check_permission(username, '4701')
+
+        if not allowed:
+            return json({'message': 'no permission'}, 403)
+
+        db_pw = User().get_password(username)
+
+        if db_pw != password:
+            return json({'message': 'Username/Password combination incorrect'},
+                        401)
+        token = jwt.encode(jwt_payload(username), config.JWT['jwtSecret'],
+                           config.JWT['algorithm'])
+
+        resp = json({'username': username, 'token': token}, 200)
+
+    except KeyError as e:
+        return json({'message': 'Username/Password combination incorrect'},
+                    401)
+    return resp
