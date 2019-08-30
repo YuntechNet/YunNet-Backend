8e45c509c0bd31be7e43aaa9ad0479a04c141653
Zeinok <zeinok@protonmail.com>
2019-08-31 07:56:23 +0800

    Fix indent

--- api/user/ip.py
+++ api/user/ip.py
@@ -64,14 +64,14 @@ async def bp_ip_get_owned_ip(request, username):
     for ip in ips:
         for key in remove_key_list:
             ip.pop(key)
-            status = ip.pop("lock_id")
-            if status is None:
-                ip["lock_status"] = "UNLOCKED"
-            else:
-                ip["lock_status"] = "LOCKED"
-            status = ip.pop("is_unlimited")
-            if status == 1:
-                ip["lock_status"] = "UNLIMITED"
+        status = ip.pop("lock_id")
+        if status is None:
+            ip["lock_status"] = "UNLOCKED"
+        else:
+            ip["lock_status"] = "LOCKED"
+        status = ip.pop("is_unlimited")
+        if status == 1:
+            ip["lock_status"] = "UNLIMITED"
 
     response = json(ips)
     return response
