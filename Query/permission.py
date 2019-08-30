d1aba65c25f987202eb6386c5b4ca8bff53fb32e
Zeinok <zeinok@protonmail.com>
2019-08-31 05:34:36 +0800

    Verify permission from JWT payload

--- Query/permission.py
+++ Query/permission.py
@@ -32,3 +32,23 @@ class Permission:
                     return True
                 else:
                     return False
+
+    @staticmethod
+    async def get_all_permission(username) -> tuple:
+        sql = (
+            "SELECT p.str FROM `user` AS u "
+            "INNER JOIN `group_user` AS gu ON u.uid = gu.uid "
+            "INNER JOIN `group` AS g on gu.gid = g.gid "
+            "INNER JOIN `group_permission` AS gp ON gp.gid = g.gid "
+            "INNER JOIN `permission` AS p ON p.pid = gp.pid "
+            "WHERE u.username = %s"
+        )
+        async with SQLPool.acquire() as conn:
+            async with conn.cursor() as cur:
+                await cur.execute(sql, username)
+                permission_list_list = await cur.fetchall()
+                permission_list = []
+                for p in permission_list_list:
+                    permission_list.append(p[0])
+                return permission_list
+
