00ac6ebf72a032474edb805fa2b7a4bc9d8bb0fd
biboy1999 <biboy1999@gmail.com>
2019-09-07 00:48:07 +0800

    fix bed-change don't have endpoint
    move user add/leave to bed-change endpoint

--- api/management/bed_change.py
+++ api/management/bed_change.py
@@ -3,8 +3,10 @@ from sanic_openapi import api, doc
 
 from Base import messages
 from Decorators import permission
+from Query import User
 from Query.bed import Bed
 from Query.ip import Ip
+from Query.userinfo import Userinfo
 
 bp_bed_change = Blueprint("management-bed-change")
 
@@ -42,10 +44,11 @@ class change_bed_doc(api.API):
 
     response = [SuccessResp, FailResp]
 
+
 @change_bed_doc
-@bp_bed_change.route("/", methods=["POST"])
-@permission("system.dormitory.bed.exchange")
-async def change_bed(request):
+@bp_bed_change.route("/bed-change", methods=["PUT"])
+@permission("api.bed.exchange")
+async def exchange_bed(request):
     source_bed = request.json["source_bed"]
     dest_bed = request.json["dest_bed"]
     # for validate uid
@@ -63,4 +66,96 @@ async def change_bed(request):
     await Ip.assign_user(dest_ip[0]["ip"], source_uid)
     await Ip.assign_user(source_ip[0]["ip"], dest_uid)
 
+    return messages.OPERATION_SUCCESS
+
+
+class delete_user_doc(api.API):
+    class SuccessResp:
+        code = 200
+        description = "On success login"
+
+        class model:
+            username = doc.String("Username")
+            token = doc.String("User's jwt token")
+
+        model = dict(vars(model))
+
+    class FailResp:
+        code = 400
+        description = "On failed login"
+
+        class model:
+            message = doc.String("Error message")
+
+        model = dict(vars(model))
+
+    response = [SuccessResp, FailResp]
+
+
+@delete_user_doc
+@bp_bed_change.route("/bed-change/<username>", methods=["DELETE"])
+@permission("api.bed.leave")
+async def bp_delete(request, username):
+    user = await Userinfo.get_userinfo(username)
+    if user is not None:
+        return messages.USER_NOT_EXIST
+
+    await User.delete_user(username)
+
+    return messages.OPERATION_SUCCESS
+
+
+class add_user_doc(api.API):
+    consumes_content_type = "application/json"
+    consumes_location = "body"
+    consumes_required = True
+
+    class consumes:
+        username = doc.String("Username")
+        nick = doc.String("User's name")
+        department = doc.String("User's department")
+        back_mail = doc.String("User's backup mail")
+        note = doc.String("Additional note")
+
+    consumes = doc.JsonBody(vars(consumes))
+
+    class SuccessResp:
+        code = 200
+        description = "On success login"
+
+        class model:
+            username = doc.String("Username")
+            token = doc.String("User's jwt token")
+
+        model = dict(vars(model))
+
+    class FailResp:
+        code = 400
+        description = "On failed login"
+
+        class model:
+            message = doc.String("Error message")
+
+        model = dict(vars(model))
+
+    response = [SuccessResp, FailResp]
+
+
+@add_user_doc
+@bp_bed_change.route("/bed-change/<username>", methods=["POST"])
+@permission("api.bed.add")
+async def bp_add_user(request, username):
+    user = await Userinfo.get_userinfo(username)
+    if user is not None:
+        return messages.USER_ALREADY_EXIST
+
+    username = request.json["username"]
+    nick = request.json["nick"]
+    department = request.json["department"]
+    back_mail = request.json["back_mail"]
+    note = request.json["note"]
+
+    affected = await User.add_user(username, nick, department, back_mail, note)
+    if affected == 0:
+        return messages.INTERNAL_SERVER_ERROR
     return messages.OPERATION_SUCCESS
--- api/management/user/manage.py
+++ api/management/user/manage.py
@@ -9,59 +9,9 @@ from Query.userinfo import Userinfo
 bp_user_manage = Blueprint("management-user-manage")
 
 
-class add_user_doc(api.API):
-    consumes_content_type = "application/json"
-    consumes_location = "body"
-    consumes_required = True
-
-    class consumes:
-        username = doc.String("Username")
-        nick = doc.String("User's name")
-        department = doc.String("User's department")
-        back_mail = doc.String("User's backup mail")
-        note = doc.String("Additional note")
-
-    consumes = doc.JsonBody(vars(consumes))
-
-    class SuccessResp:
-        code = 200
-        description = "On success login"
-
-        class model:
-            username = doc.String("Username")
-            token = doc.String("User's jwt token")
-
-        model = dict(vars(model))
-
-    class FailResp:
-        code = 400
-        description = "On failed login"
-
-        class model:
-            message = doc.String("Error message")
-
-        model = dict(vars(model))
-
-    response = [SuccessResp, FailResp]
-
-
-@add_user_doc
-@bp_user_manage.route("/<username>", methods=["POST"])
-async def bp_add_user(request, username):
-    user = await Userinfo.get_userinfo(username)
-    if user is not None:
-        return messages.USER_ALREADY_EXIST
-
-    username = request.json["username"]
-    nick = request.json["nick"]
-    department = request.json["department"]
-    back_mail = request.json["back_mail"]
-    note = request.json["note"]
-
-    affected = await User.add_user(username, nick, department, back_mail, note)
-    if affected == 0:
-        return messages.INTERNAL_SERVER_ERROR
-    return messages.OPERATION_SUCCESS
+# @bp_user_manage.route("/<username>", methods=["POST"])
+# async def bp_add_user(request, username):
+#     pass
 
 
 # TODO(biboy1999): Low priority
@@ -71,36 +21,6 @@ async def bp_add_user(request, username):
 #     return response
 
 
-class delete_user_doc(api.API):
-    class SuccessResp:
-        code = 200
-        description = "On success login"
-
-        class model:
-            username = doc.String("Username")
-            token = doc.String("User's jwt token")
-
-        model = dict(vars(model))
-
-    class FailResp:
-        code = 400
-        description = "On failed login"
-
-        class model:
-            message = doc.String("Error message")
-
-        model = dict(vars(model))
-
-    response = [SuccessResp, FailResp]
-
-
-@delete_user_doc
-@bp_user_manage.route("/<username>", methods=["DELETE"])
-async def bp_delete(request, username):
-    user = await Userinfo.get_userinfo(username)
-    if user is not None:
-        return messages.USER_NOT_EXIST
-
-    await User.delete_user(username)
-
-    return messages.OPERATION_SUCCESS
+# @bp_user_manage.route("/<username>", methods=["DELETE"])
+# async def bp_delete(request, username):
+#     pass
